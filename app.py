e3faeeb10639476ec82d6b4efb61f64a4766bba3
jonnych6 <45064739+jonnych6@users.noreply.github.com>
2024-07-15 21:13:28 -0400

    finished SOC1, starting on GDPR

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('home1.html')
 
+@app.route('/SOC1')
+def soc1():
+    return render_template('individual_certs/SOC1.html')
+
+@app.route('/GDPR')
+def GDPR():
+    return render_template('individual_certs/GDPR.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
